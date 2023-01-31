72add821092994d8dc6907ab7924851e8c8f5ff5
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-31 13:56:03 -0300

    Estrutura da aplicação - Criando aplicação

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
