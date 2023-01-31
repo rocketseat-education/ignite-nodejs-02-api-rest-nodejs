2e3d139adbd73465f5150d436699ae6f124fa901
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-01-31 14:03:26 -0300

    Estrutura da aplicação - Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server Running!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!')
+  })
