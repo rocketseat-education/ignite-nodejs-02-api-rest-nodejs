851b86c1c1c6670c595f1a524e0499149a66617a
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-02-01 16:44:45 -0300

    Testes automatizados - Categorizando os testes

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import { afterAll, beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+})
